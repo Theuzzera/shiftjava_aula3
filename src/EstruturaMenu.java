a11f6a27ccbca71e7f18797e0855b27f20133271
matheus henrique <Matheus@fiap.com.br>
2024-03-14 22:13:03 -0300

    Refazer execicio!

---
+++ src/EstruturaMenu.java
@@ -0,0 +1,13 @@
+import com.sun.source.tree.WhileLoopTree;
+
+import java.util.Optional;
+import java.util.Scanner;
+
+public class EstruturaMenu {
+
+    public static void main(String[] args) {
+
+        //REFAZER EXERCICIO DE ESCOLHA DAS OPÇÕES DO SWITCH CASE ATÉ QUE O USUÁRIO QUEIRA SAIR
+
+    }
+}
